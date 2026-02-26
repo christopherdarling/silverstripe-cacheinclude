ebb2d9618d1bb889bb97eaecbcb450ffcbe99268
Michael Houghton <michael.houghton@oireachtas.ie>
2026-02-26 14:24:31 +0000

    Fix for depreciation error

--- src/KeyCreators/ControllerBased.php
+++ src/KeyCreators/ControllerBased.php
@@ -39,7 +39,7 @@ class ControllerBased implements KeyCreatorInterface, KeyInformationProviderInte
      * @param  Controller|void $controller
      * @throws \Exception
      */
-    public function __construct(Controller $controller = null)
+    public function __construct(Controller $controller)
     {
         if (!$controller && !Controller::has_curr()) {
             throw new \Exception("Controller based key creators must have a current controller");
